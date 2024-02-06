054740e7dcc8f292723ce7f13186b3ee409aebe4
raman340 <153404687+raman340@users.noreply.github.com>
2024-02-06 03:31:16 +0000

    Add files via upload

--- starter_file/train.py
+++ starter_file/train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import argparse
 
 import os  
 
@@ -8,69 +9,47 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import MinMaxScaler
 
 from azureml.core.run import Run
-from azureml.core import Workspace
-from azureml.data.dataset_factory import TabularDatasetFactory
+# from azureml.data.dataset_factory import TabularDatasetFactory
+import joblib
 
 def clean_data(data):
       # Create a copy of the original dataframe
     heart_clean_df = data.copy()
 
     # Impute 0 values in RestingBP with median value of the column grouped by HeartDisease
-    heart_clean_df['RestingBP'] = heart_clean_df.groupby('HeartDisease')['RestingBP'].apply(lambda x: x.replace(0, x.median()))
+    #heart_clean_df['RestingBP'] = heart_clean_df.groupby('HeartDisease')['RestingBP'].apply(lambda x: x.replace(0, x.median()))
 
     # Impute 0 values in Cholesterol with median value of the column grouped by HeartDisease
-    heart_clean_df['Cholesterol'] = heart_clean_df.groupby('HeartDisease')['Cholesterol'].apply(lambda x: x.replace(0, x.median()))
+    #heart_clean_df['Cholesterol'] = heart_clean_df.groupby('HeartDisease')['Cholesterol'].apply(lambda x: x.replace(0, x.median()))
 
     # Convert categorical variable into dummy variables
     heart_clean_df = pd.get_dummies(heart_clean_df, drop_first=True)
+    
+    print(heart_clean_df.columns)
              
     # Split data into features 'X' and target variable 'y'
-    # X = heart_clean_df#.drop('HeartDisease', axis=1)
-    X = heart_clean_df
+    X = heart_clean_df#.drop('HeartDisease', axis=1)
+    #X = heart_clean_df
     y = heart_clean_df['HeartDisease']
     
     return X, y
 
 
 def main():
-    ws1 = Workspace.from_config()
     
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--n_neighbors', type=int, default=5, help="The number of neighbors to consider")
+    parser.add_argument('--weights', type=str, default='uniform', help="Maximum number of iterations to converge")
+    parser.add_argument('--metric', type=str, default='minkowski', help="The distance metric used for the tree.")
+    
+    args = parser.parse_args()
     #
     run = Run.get_context()
     
-        # Try to load the dataset from the Workspace. Otherwise, create it from the file
-    # NOTE: update the key to match the dataset name
-    found = False
-    key = "HeartFailurePrediction"
-    description_text = "Heart Failure Prediction Dataset Kaggle"
-
-    if key in ws1.datasets.keys(): 
-            found = True
-            dataset = ws1.datasets[key] 
-
-    if not found:      
-            # Set Kaggle credentials as environment variables or in a separate Kaggle.json file  
-            os.environ['KAGGLE_USERNAME'] = '<your_kaggle_username>'  
-            os.environ['KAGGLE_KEY'] = '<your_kaggle_api_key>'  
-
-            # kaggle datasets download -d fedesoriano/heart-failure-prediction
-
-            # Download the Kaggle dataset using the KaggleDatasetFactory  
-            from azureml.data.dataset_factory import KaggleDatasetFactory  
-
-            factory = KaggleDatasetFactory()  
-            dataset = factory.create_dataset(  
-                dataset_id='<kaggle_dataset_id>',  
-                version='<kaggle_dataset_version>'  
-            )  
-
-            #Register Dataset in Workspace
-            dataset = dataset.register(workspace=ws1pa,
-                                       name=key,
-                                       description=description_text)
-   
-    # Convert to Pandas DF
-    heart_disease_df = dataset.to_pandas_dataframe()
+    heart_disease_df = pd.read_csv('data.csv')
+    #heart_disease_df = pd.read_csv('heart_disease_prediction.csv')
     
     # Clean data
     X, y = clean_data(heart_disease_df)
@@ -87,7 +66,7 @@ def main():
     # Split data for training and validation sets
     X_train, X_val, y_train, y_val = train_test_split(X[sel_features], y,
                                                       test_size=0.15,
-                                                      random_state=417)
+                                                      random_state=0)
 
     # Instantiate scaler
     scaler = MinMaxScaler()
@@ -99,14 +78,17 @@ def main():
     X_val_scaled = scaler.transform(X_val)
 
     # Fit the model on scaled features (using default n_neighbors value)
-    knn = KNeighborsClassifier()
+    knn = KNeighborsClassifier(n_neighbors=args.n_neighbors, weights=args.weights, metric=args.metric)
     knn.fit(X_train_scaled, y_train)
 
     # Evaluate the model on scaled features
     accuracy = knn.score(X_val_scaled, y_val)
 
     print(f'Model accuracy: {accuracy*100:.2f}%')
-    #run.log("Accuracy", np.float(accuracy))
+    run.log("Accuracy", float(accuracy))
+    
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(knn, 'outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
